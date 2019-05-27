9bb5f0f0f63e2588addce843a26991f7d1eef653
Dencer <tdaddon@163.com>
2019-05-27 18:53:32 +0800

    feat: add Frame.show Frame.hide Frame.setFocus

--- frame.lua
+++ frame.lua
@@ -1,1 +1,19 @@
-return require('lib.oop.generated.frame')
+local Frame = require('lib.oop.generated.frame')
+local Native = require('lib.native.native')
+
+---show
+function Frame:show()
+    return self:setVisible(true)
+end
+
+---hide
+function Frame:hide()
+    return self:setVisible(false)
+end
+
+---clearFocus
+function Frame:clearFocus()
+    return self:setFocus(true)
+end
+
+return Frame
