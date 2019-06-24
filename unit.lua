abc4610de107e1844eb026d4dee0ec51e460b4c0
Dencer <tdaddon@163.com>
2019-06-24 21:34:35 +0800

    feat: Add Unit.isDead Unit.isAlive

--- unit.lua
+++ unit.lua
@@ -1,1 +1,13 @@
-return require('lib.oop.generated.unit')
+local Unit = require('lib.oop.generated.unit')
+
+---isDead
+---@return boolean
+function Unit:isDead()
+    return self:getState(UnitState.Life) <= 0
+end
+
+---isAlive
+---@return boolean
+function Unit:isAlive()
+    return not self:isDead()
+end
