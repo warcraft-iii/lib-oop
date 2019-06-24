749bb03279ba0094ad23d987192c55af461a84af
Dencer <tdaddon@163.com>
2019-06-25 00:24:44 +0800

    feat: Add Player.adjustState Player.adjustGold Player.adjustLumber

--- player.lua
+++ player.lua
@@ -1,1 +1,19 @@
-return require('lib.oop.generated.player')
+local Player = require('lib.oop.generated.player')
+
+---adjustState
+---@param state PlayerState
+---@param delta integer
+---@return void
+function Player:adjustState(state, delta)
+    return self:setState(state, self:getState(state) + delta)
+end
+
+function Player:adjustGold(delta)
+    return self:adjustState(PlayerState.GoldGathered, delta)
+end
+
+function Player:adjustLumber(delta)
+    return self:adjustState(PlayerState.LumberGathered, delta)
+end
+
+return Player
