93aa2c1026363b2712d3da6230e9ef8e8a3d1486
Dencer <tdaddon@163.com>
2019-06-25 23:02:46 +0800

    feat: Add Trigger.registerAllPlayersUnitEvent

--- trigger.lua
+++ trigger.lua
@@ -1,1 +1,12 @@
-return require('lib.oop.generated.trigger')
+local Trigger =  require('lib.oop.generated.trigger')
+local Player = require('lib.oop.player')
+
+---registerAllPlayersUnitEvent
+---@param playerUnitEvent PlayerUnitEvent
+function Trigger:registerAllPlayersUnitEvent(playerUnitEvent)
+    for i = 0, bj_MAX_PLAYER_SLOTS - 1 do
+        self:registerPlayerUnitEvent(Player:get(i), playerUnitEvent)
+    end
+end
+
+return Trigger
